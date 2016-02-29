7392661195cd2f70644af74ecffd58c88de9c4b9
Erik Dungan <erik.dungan@gmail.com>
2016-02-29 11:18:58 -0800

    updated home page

--- app.rb
+++ app.rb
@@ -4,11 +4,16 @@ require 'mongo'
 require 'json'
 
 class CalPoly < Sinatra::Base
-  configure do
-    db = Mongo::Client.new([ '127.0.0.1:27017' ], :database => 'calpoly')
-    set :mongo_db, db[:courses]
-    # I18n.enforce_available_locales = false
-  end
+  ##
+  # This is the config block where you can enable MongoDB
+  # If you want to use MongoDB for your data (instead of the JSON files),
+  # uncomment this block and change the config settings accordingly
+  #
+  # configure do
+  #   db = Mongo::Client.new([ '127.0.0.1:27017' ], :database => 'calpoly')
+  #   set :mongo_db, db[:courses]
+  #   # I18n.enforce_available_locales = false
+  # end
 
   # This is just the boring home page
   get '/' do
